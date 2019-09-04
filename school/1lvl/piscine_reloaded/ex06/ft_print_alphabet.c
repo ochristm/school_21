d189bde3ebab263ad2f2d4c5a59901980c87ddc1
Попова Кристина Витальевна <Popova_KV@mgtniip.ru>
2019-09-04 12:15:46 +0300

    add ft

---
+++ school/1lvl/piscine_reloaded/ex06/ft_print_alphabet.c
@@ -0,0 +1,18 @@
+// HEADER!!!!
+// REMOVE FT_PUTCHAR - ONLY PROTOTYPE
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c >= 'a' && c <= 'z')
+		ft_putchar(c++);
+}
