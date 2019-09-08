6f172231e0e236cf26c4f7404ba4ca9d3887f6c0
ochristm <kristina9601@mail.ru>
2019-09-08 16:38:32 +0300

    add ft

---
+++ 2libft/srcs/ft_itoa.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ochristm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/08 15:08:50 by ochristm          #+#    #+#             */
+/*   Updated: 2019/09/08 16:37:01 by ochristm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_nlen(int n, int base)
+{
+	size_t len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0 && base == 10)
+		len++;
+	while (n)
+	{
+		n /= base;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	size_t	i;
+	size_t	n_len;
+	char	*str;
+
+	i = 0;
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	n_len = ft_nlen(n, 10);
+	if (!(str = (char *)malloc(sizeof(char) * (n_len + 1))))
+		return (NULL);
+	if (n < 0)
+	{
+		str[0] = '0';
+		n = -n;
+		i++;
+	}
+	while (i < n_len--)
+	{
+		str[n_len] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
+}
