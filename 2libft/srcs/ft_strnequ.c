7abd82f6abe58d6b5b96682ca96ed774db1f1a10
ochristm <kristina9601@mail.ru>
2019-09-09 19:20:33 +0300

    upd

--- 2libft/srcs/ft_strnequ.c
+++ 2libft/srcs/ft_strnequ.c
@@ -6,13 +6,13 @@
 /*   By: ochristm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 13:00:48 by ochristm          #+#    #+#             */
-/*   Updated: 2019/09/08 13:03:41 by ochristm         ###   ########.fr       */
+/*   Updated: 2019/09/09 19:20:22 by ochristm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strnequ(char const *s1, char const *s2)
+int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	if (s1 && s2)
 		if (!n || !ft_strncmp(s1, s2, n))
