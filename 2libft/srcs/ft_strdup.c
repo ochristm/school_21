3154ff11ad6dd5084edea96cb164c423643e2520
ochristm <kristina9601@mail.ru>
2019-09-09 18:30:44 +0300

    ui

--- 2libft/srcs/ft_strdup.c
+++ 2libft/srcs/ft_strdup.c
@@ -6,11 +6,11 @@
 /*   By: ochristm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 21:05:54 by ochristm          #+#    #+#             */
-/*   Updated: 2019/09/05 21:06:13 by ochristm         ###   ########.fr       */
+/*   Updated: 2019/09/09 18:30:21 by ochristm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strdup(char *src)
 {
