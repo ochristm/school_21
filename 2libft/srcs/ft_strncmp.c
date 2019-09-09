2baa4c9eba00d336ca7d6b2299026ce176f91f5a
ochristm <kristina9601@mail.ru>
2019-09-09 19:19:09 +0300

    upd

--- 2libft/srcs/ft_strncmp.c
+++ 2libft/srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ochristm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/07 19:41:04 by ochristm          #+#    #+#             */
-/*   Updated: 2019/09/07 19:50:41 by ochristm         ###   ########.fr       */
+/*   Updated: 2019/09/09 19:19:01 by ochristm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (!n)
 		return (0);
 	ucs1 = (unsigned char *)s1;
-	usc2 = (unsigned char *)s2;
+	ucs2 = (unsigned char *)s2;
 	while ((*ucs1 && (*ucs1 == *ucs2)) && --n)
 	{
-		if (*ucs != *ucs2)
+		if (*ucs1 != *ucs2)
 			return (*ucs1 - *ucs2);
 		ucs1++;
 		ucs2++;
 	}
-	return (*ucs1 - ucs2);
+	return (*ucs1 - *ucs2);
 }
