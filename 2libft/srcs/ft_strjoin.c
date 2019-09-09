00d80061cc0f28e429902eec18c4d63d53681f4f
ochristm <kristina9601@mail.ru>
2019-09-09 19:15:20 +0300

    upd

--- 2libft/srcs/ft_strjoin.c
+++ 2libft/srcs/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ochristm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 13:13:19 by ochristm          #+#    #+#             */
-/*   Updated: 2019/09/08 13:19:46 by ochristm         ###   ########.fr       */
+/*   Updated: 2019/09/09 19:15:11 by ochristm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char *s;
 
-	str = NULL;
+	s = NULL;
 	if (s1 && s2)
 	{
 		s = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
